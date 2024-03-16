d4ca6e7dbad479ed777ec46d6b5d2e3587219e3c
Marcel Hinderlich <m.hinderlich@reply.de>
2024-03-16 01:12:53 +0100

    added step1 and styling

--- forms-example/src/app/wizard-steps/step1.component.ts
+++ forms-example/src/app/wizard-steps/step1.component.ts
@@ -7,10 +7,88 @@ import {FormWizardService} from "../form-wizard/form-wizard.service";
   selector: 'step1',
   template: `
     <form [formGroup]="form">
-      <div>
-        <label>Name</label>
-        <input type="text" formControlName="name">
+      <div class="flex">
+        <div class="half-containers">
+          <div class="form-group flex">
+            <label>Salutation</label>
+            <select formControlName="title">
+              <option>Mr</option>
+              <option>Mrs</option>
+            </select>
+          </div>
+          <div class="form-group flex">
+            <label>Name</label>
+            <input type="text" formControlName="name">
+          </div>
+          <div class="form-group flex">
+            <label>Prename</label>
+            <input type="text" formControlName="prename">
+          </div>
+          <div class="form-group flex">
+            <label>Birthdate</label>
+            <input type="date"/>
+          </div>
+        </div>
+        <div class="half-containers">
+
+          <div class="form-group flex">
+            <label>Title</label>
+            <select>
+              <option>One</option>
+              <option>Two</option>
+            </select>
+          </div>
+
+          <div class="form-group flex">
+            <label>Name Suffix</label>
+            <input type="text" formControlName="name suffix">
+          </div>
+
+        </div>
+
       </div>
+
+
+      <hr/>
+
+      <div class="flex">
+        <div class="half-containers">
+          <div class="form-group flex">
+            <label>Street</label>
+            <input type="text" formControlName="street">
+          </div>
+          <div class="form-group flex">
+            <label>Zip code</label>
+            <input type="text" formControlName="zipcode">
+          </div>
+        </div>
+
+        <div class="half-containers">
+
+          <div class="form-group flex">
+            <label>House number</label>
+            <input type="number">
+          </div>
+
+          <div class="form-group flex">
+            <label>Location</label>
+            <input type="text" formControlName="location">
+          </div>
+
+          <div class="form-group flex">
+            <label>Country</label>
+            <select>
+              <option disabled selected value> -- select -- </option>
+s              <option>Germany</option>
+              <option>Austria</option>
+            </select>
+          </div>
+
+        </div>
+
+      </div>
+
+
     </form>
   `
 })
