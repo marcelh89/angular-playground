b19ee7b8a2b4672b9e9f46130db48566edc8acf8
Marcel Hinderlich <m.hinderlich@reply.de>
2024-03-16 02:06:10 +0100

    added marker for required fields

--- forms-example/src/app/wizard-steps/step1.component.ts
+++ forms-example/src/app/wizard-steps/step1.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import {Component} from "@angular/core";
+import {FormControl, Validators} from "@angular/forms";
 import {FormWizardStepBaseComponent} from "../form-wizard/form-wizard-step-base.component";
 import {FormWizardService} from "../form-wizard/form-wizard.service";
 
@@ -11,38 +11,62 @@ import {FormWizardService} from "../form-wizard/form-wizard.service";
       <div class="flex">
         <div class="half-containers">
           <div class="form-group flex">
-            <label>Salutation</label>
+            <label>Salutation
+              @if (this.formcontrols.salutation?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <select formControlName="salutation">
               <option>Mr</option>
               <option>Mrs</option>
             </select>
           </div>
           <div class="form-group flex">
-            <label>Name</label>
+            <label>Name
+              @if (this.formcontrols.name?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="text" formControlName="name">
           </div>
           <div class="form-group flex">
-            <label>Prename</label>
+            <label>Prename
+              @if (this.formcontrols.prename?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="text" formControlName="prename">
           </div>
           <div class="form-group flex">
-            <label>Birthdate</label>
+            <label>Birthdate
+              @if (this.formcontrols.birthdate?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="date" formControlName="birthdate"/>
           </div>
         </div>
         <div class="half-containers">
 
           <div class="form-group flex">
-            <label>Title</label>
-            <select>
+            <label>Title
+              @if (this.formcontrols.title?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
+            <select formControlName="title">
               <option>One</option>
               <option>Two</option>
             </select>
           </div>
 
           <div class="form-group flex">
-            <label>Name Suffix</label>
-            <input type="text" formControlName="name suffix">
+            <label>Name Suffix
+              @if (this.formcontrols.namesuffix?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
+            <input type="text" formControlName="namesuffix">
           </div>
 
         </div>
@@ -55,11 +79,19 @@ import {FormWizardService} from "../form-wizard/form-wizard.service";
       <div class="flex">
         <div class="half-containers">
           <div class="form-group flex">
-            <label>Street</label>
+            <label>Street
+              @if (this.formcontrols.street?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="text" formControlName="street">
           </div>
           <div class="form-group flex">
-            <label>Zip code</label>
+            <label>Zip code
+              @if (this.formcontrols.zipcode?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="text" formControlName="zipcode">
           </div>
         </div>
@@ -67,20 +99,32 @@ import {FormWizardService} from "../form-wizard/form-wizard.service";
         <div class="half-containers">
 
           <div class="form-group flex">
-            <label>House number</label>
-            <input type="number">
+            <label>House number
+              @if (this.formcontrols.housenumber?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
+            <input type="number" formControlName="housenumber">
           </div>
 
           <div class="form-group flex">
-            <label>Location</label>
+            <label>Location
+              @if (this.formcontrols.location?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
             <input type="text" formControlName="location">
           </div>
 
           <div class="form-group flex">
-            <label>Country</label>
-            <select>
-              <option disabled selected value> -- select -- </option>
-s              <option>Germany</option>
+            <label>Country
+              @if (this.formcontrols.country?.hasValidator(Validators.required)) {
+                <span>*</span>
+              }
+            </label>
+            <select formControlName="country">
+              <option disabled selected value> -- select --</option>
+              <option>Germany</option>
               <option>Austria</option>
             </select>
           </div>
@@ -95,15 +139,25 @@ s              <option>Germany</option>
 })
 export class Step1Component extends FormWizardStepBaseComponent {
 
+  formcontrols: any;
+
   constructor(private wizardService: FormWizardService) {
     const formcontrols = {
-      'name':  new FormControl('', [ Validators.required ]),
-      'salutation':  new FormControl('', [ Validators.required ]),
-      'prename':  new FormControl('', [ Validators.required ]),
-      'birthdate':  new FormControl('', [ Validators.required ]),
-
+      'name': new FormControl('', [Validators.required]),
+      'salutation': new FormControl('', [Validators.required]),
+      'prename': new FormControl('', []),
+      'birthdate': new FormControl('', []),
+      'street': new FormControl('', []),
+      'namesuffix': new FormControl('', []),
+      'title': new FormControl('', []),
+      'housenumber': new FormControl('', []),
+      'location': new FormControl('', []),
+      'zipcode': new FormControl('', []),
+      'country': new FormControl('', []),
     };
     super(1, wizardService.getSteps(), true, formcontrols);
+    this.formcontrols = formcontrols;
   }
 
+  protected readonly Validators = Validators;
 }
