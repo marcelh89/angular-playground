23974d3dedfb15b2cf957f6fd64d3a751bc2734e
Marcel Hinderlich <m.hinderlich@reply.de>
2024-03-22 15:14:53 +0100

    added angular materialui, use material buttons

--- wizard-form/src/app/multistep-form/multistep-form.component.ts
+++ wizard-form/src/app/multistep-form/multistep-form.component.ts
@@ -5,12 +5,14 @@ import {AppStore} from './multistep-form.store'
 import {StepComponent} from './step/step.component'
 import {SidebarComponent} from './sidebar/sidebar.component'
 import { HttpClientModule } from '@angular/common/http';
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
 
 
 @Component({
   selector: 'app-multistep-form',
   standalone: true,
-  imports: [CommonModule,NgIf,StepComponent, SidebarComponent, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule,NgIf,StepComponent, SidebarComponent, ReactiveFormsModule, HttpClientModule, MatIcon, MatButton],
   templateUrl: './multistep-form.component.html',
 })
 export class MultistepFormComponent {
