2a7feea0e9ff5a1138c667309150d3ca3dbe0cb3
Marcel Hinderlich <m.hinderlich@reply.de>
2024-03-22 15:22:47 +0100

    use mat-toolbar

--- wizard-form/src/app/app.component.ts
+++ wizard-form/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component,  } from '@angular/core';
 import {MultistepFormComponent} from './multistep-form/multistep-form.component'
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MultistepFormComponent],
+  imports: [MultistepFormComponent, MatToolbar],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
